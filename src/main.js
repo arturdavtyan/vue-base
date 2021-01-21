92c959e3f0a30a1133ec33b8a66def29798c549e
Artur <arturdavtyanars@gmail.com>
2021-01-21 17:49:37 +0400

    Initialize git

---
+++ src/main.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+// Styles: SCSS
+import './assets/scss/main.scss'
+
+// Globally Registered Components
+import './utils/GlobalComponents'
+
+// VeeValidate
+import VeeValidate from 'vee-validate'
+
+// Vue Router
+import router from './router'
+
+// Vuex store
+import store from './store'
+
+Vue.use(VeeValidate)
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,48 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import store from '../store/'
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    // Main layout
+    {
+      path: '/',
+      component: () => import('@/layouts/Main.vue')
+    },
+
+    // Full Page layout
+    {
+      path: '/',
+      component: () => import('@/layouts/FullPage.vue'),
+      children: [
+        {
+          path: 'login',
+          name: 'login',
+          component: () => import('@/views/Auth/Login/Index')
+        },
+        {
+          path: 'register',
+          name: 'register',
+          component: () => import('@/views/Auth/Register/Index')
+        }
+      ]
+    }
+  ]
+})
+
+router.beforeEach((to, from, next) => {
+  const isRequiredAuth = to.matched.some(route => route.meta.requiredAuth)
+  const isAuthenticated = store.getters['Auth/IsAuthenticated']
+
+  if (isRequiredAuth && !isAuthenticated) {
+    next({ name: 'login' })
+  } else {
+    next()
+  }
+})
+
+export default router
